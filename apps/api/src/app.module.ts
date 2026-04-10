5a41ff4980ed714c483602bb592a072a9d94b770
srs2800302-collab <srs2800302@gmail.com>
2026-04-10 03:43:30 +0300

    feat: add profiles and categories modules (#7)

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 import { HealthModule } from './modules/health/health.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { ProfilesModule } from './modules/profiles/profiles.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PrismaModule, HealthModule, UsersModule, AuthModule],
+  imports: [
+    PrismaModule,
+    HealthModule,
+    UsersModule,
+    AuthModule,
+    CategoriesModule,
+    ProfilesModule,
+  ],
 })
 export class AppModule {}
