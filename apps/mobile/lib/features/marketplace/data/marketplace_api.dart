51832eca2b912ea25de05724954427e69b5cd509
srs <srs2800302@gmail.com>
2026-04-20 01:18:13 +0300

    fix(mobile): use master available jobs endpoint for marketplace

--- apps/mobile/lib/features/marketplace/data/marketplace_api.dart
+++ apps/mobile/lib/features/marketplace/data/marketplace_api.dart
@@ -10,7 +10,7 @@ class MarketplaceApi {
     required String masterUserId,
   }) async {
     final response = await apiClient.dio.get(
-      '/users/$masterUserId/available-jobs',
+      '/jobs/available',
     );
 
     final data = response.data['data'] as List<dynamic>;
